6622f32ce9cbb4a6fb96475bd6385ada57239ea1
Serhii Rybak <serhiirybak@Serhiis-MacBook-Pro.local>
2024-01-17 22:25:18 +0100

    Done task 1. Added some docker scripts

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const elements = {
+  body: document.body,
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+let timer;
 
+elements.startBtn.addEventListener('click', startBtnClickHandler);
+
+elements.stopBtn.addEventListener('click', stopBtnClickHandler);
+
+function startBtnClickHandler() {
+  elements.body.style.backgroundColor = getRandomHexColor();
+  timer = setInterval(() => {
+    elements.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  elements.startBtn.disabled = true;
+  elements.stopBtn.disabled = false;
+}
+
+function stopBtnClickHandler() {
+  clearInterval(timer);
+  elements.startBtn.disabled = false;
+  elements.stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,34 @@
-import { defineConfig } from 'vite';
-import glob from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
+import { defineConfig } from "vite";
+import glob from "glob";
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
 
 export default defineConfig(({ command }) => {
   return {
     define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+      [command === "serve" ? "global" : "_global"]: {},
     },
-    root: 'src',
+    root: "src",
+    server: {
+      host: true,
+      port: 8080,
+    },
     build: {
       sourcemap: true,
 
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: glob.sync("./src/*.html"),
         output: {
           manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
+            if (id.includes("node_modules")) {
+              return "vendor";
             }
           },
-          entryFileNames: 'commonHelpers.js',
+          entryFileNames: "commonHelpers.js",
         },
       },
-      outDir: '../dist',
+      outDir: "../dist",
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    plugins: [injectHTML(), FullReload(["./src/**/**.html"])],
   };
 });
